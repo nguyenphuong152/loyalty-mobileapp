aafa5aea4cf8b6e2d570f5a66a51fec64cf1e839
nguyenphuong <17520927@ms.uit.edu.vn>
2021-01-05 16:28:50 +0700

    update

--- loyalty_app/lib/view/welcome/login/components/body.dart
+++ loyalty_app/lib/view/welcome/login/components/body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loyalty_app/bloc/authen_bloc.dart';
 import 'package:loyalty_app/component/already_have_account_check.dart';
 import 'package:loyalty_app/component/rounded_input_field.dart';
 import 'package:loyalty_app/component/rounded_password_field.dart';
+import 'package:loyalty_app/providers/providers.dart';
 import 'package:loyalty_app/view/home/home_screen.dart';
 import 'package:loyalty_app/view/welcome/signup/signup_screen.dart';
 import 'package:loyalty_app/view/welcome/welcomee/components/background.dart';
@@ -10,13 +12,15 @@ import 'package:loyalty_app/view/welcome/welcomee/components/background.dart';
 import '../../../../constant.dart';
 
 class Body extends StatelessWidget {
-  const Body({
+  final authen = new AuthenBloc();
+  Body({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
     return Background(
       child: SingleChildScrollView(
         child: Column(
@@ -27,37 +31,47 @@ class Body extends StatelessWidget {
               height: size.height * 0.3,
             ),
             SizedBox(height: size.height * 0.03),
-            RoundedInputField(
-              hintText: "Số điện thoại",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
-            ),
+            StreamBuilder<String>(
+                stream: authen.email,
+                builder: (context, snapshot) {
+                  return RoundedInputField(
+                    hintText: "Email",
+                    onChanged: authen.changeEmail,
+                  );
+                }),
+            StreamBuilder<String>(
+                stream: authen.password,
+                builder: (context, snapshot) {
+                  return RoundedPasswordField(
+                    onChanged: authen.changePassword,
+                  );
+                }),
             SizedBox(height: size.height * 0.03),
             SizedBox(
               width: 200,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    side: BorderSide(color: mPrimaryColor)),
-                color: mPrimaryColor,
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return HomeScreen();
+              child: StreamBuilder<bool>(
+                  stream: authen.submitValidForm,
+                  builder: (context, snapshot) {
+                    return RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: BorderSide(color: mPrimaryColor)),
+                      color: mPrimaryColor,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        if (snapshot.hasError) {
+                          print(snapshot.error);
+                          return null;
+                        }
+                        authen.login(context);
                       },
-                    ),
-                  );
-                },
-                child: Text(
-                  "đăng nhập".toUpperCase(),
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400),
-                ),
-              ),
+                      child: Text(
+                        "đăng nhập".toUpperCase(),
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w400),
+                      ),
+                    );
+                  }),
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
